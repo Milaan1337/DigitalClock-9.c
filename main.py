52cbce9a726c68de6f7c07aafba99bab590f45c2
Asus <zs.florian25@gmail.com>
2021-03-30 13:57:10 +0200

    nulla es negy

--- main.py
+++ main.py
@@ -28,15 +28,15 @@ class Kattintgato:
         self.t.forward(20)
         self.t.end_fill()
 
-    #def asd2(self):
-        #for i in range(3):
-            #self.t.left(135)
-            #self.t.forward(100)
-            #self.t.left(90)
+    def asd2(self):
+        for i in range(3):
+            self.t.left(135)
+            self.t.forward(100)
+            self.t.left(90)
 
-    #def asd3(self):
-        #self.t.right(135)
-        #self.asd()
+    def asd3(self):
+        self.t.right(135)
+        self.asd()
 
     def nulla(self):
         self.t.left(90)
@@ -71,9 +71,6 @@ class Kattintgato:
 
 
     def egy(self):
-        x = self.t.xcor()
-        y = self.t.ycor()
-        heading = self.t.heading()
         self.t.left(90)
         self.asd()
         self.t.right(45)
@@ -81,15 +78,8 @@ class Kattintgato:
         self.t.forward(15)
         self.t.pendown()
         self.asd()
-        self.t.penup()
-        self.t.goto(x,y)
-        self.t.pendown()
-        self.t.setheading(heading)
 
     def ketto(self):
-        x2 = self.t.xcor()
-        y2 = self.t.ycor()
-        heading2 = self.t.heading()
         self.asd()
         for i in range(2):
             self.t.right(135)
@@ -102,57 +92,69 @@ class Kattintgato:
             self.t.penup()
             self.t.forward(15)
             self.asd()
-        self.t.goto(x2,y2)
-        self.t.setheading(heading2)
 
     def harom(self):
-        x3 = self.t.xcor()
-        y3 = self.t.ycor()
-        heading3 = self.t.heading()
+        self.asd()
+        for i in range(2):
+            self.t.right(135)
+            self.t.penup()
+            self.t.forward(15)
+            self.t.pendown()
+            self.asd()
+        for i in range(2):
+            self.t.right(45)
+            self.t.penup()
+            self.t.forward(15)
+            self.asd()
+
+    def negy(self):
+        self.t.left(90)
+        self.asd()
+        self.t.right(45)
+        self.t.penup()
+        self.t.forward(10)
+        self.t.left(90)
+        self.t.forward(10)
+        self.t.pendown()
         self.asd()
         self.t.right(135)
         self.t.penup()
-        self.t.forward(15)
+        self.t.forward(10)
+        self.t.forward(10)
         self.t.pendown()
         self.asd()
-
-        self.t.right(45)
-        self.t.penup()
-        self.t.forward(15)
-        self.asd()
-
-        self.t.left(180)
-        self.t.right(45)
+        self.t.right(135)
         self.t.penup()
         self.t.forward(90)
+        self.t.right(90)
+        self.t.forward(10)
+        self.asd()
+
+    def ot(self):
         self.t.left(90)
-        self.t.forward(15)
-
         self.asd()
-
-        self.t.left(135)
+        self.t.right(45)
         self.t.penup()
-        self.t.forward(95)
-        self.t.right(90)
-        self.t.forward(90)
-        self.t.right(90)
-        self.t.forward(15)
+        self.t.forward(10)
+        self.t.left(90)
+        self.t.forward(10)
+        self.t.pendown()
         self.asd()
-        self.t.goto(x3,y3)
-        self.t.setheading(heading3)
+        self.t.right(135)
+        self.t.penup()
+        self.t.forward(10)
+        self.t.forward(10)
+        self.t.pendown()
+        self.asd()
+        self.t.right(45)
+        self.t.forward()
 
-    def secondleft(self):
-        if self.c.leftNumber(self.c.sec()) == 1:
-            self.egy()
-        if self.c.leftNumber(self.c.sec()) == 2:
-            self.ketto()
-        if self.c.leftNumber(self.c.sec()) == 3:
-            self.harom()
+
 
     def __init__(self):
         self.t.fillcolor("yellow")
         self.t2.fillcolor("yellow")
-        self.nulla()
+        self.ot()
 
         self.t._delay(0)
         self.t.speed(100000000000)
